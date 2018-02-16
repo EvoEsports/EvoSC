4aef807251048720d5898dbc7f237b82a1e01fa8
Alex 'Braker' R <brakerb@vination.eu>
2018-02-16 14:14:22 +0100

    0.7.1
    -Make default groups protected from deletion

--- core/controllers/GroupController.php
+++ core/controllers/GroupController.php
@@ -16,15 +16,16 @@ class GroupController
     private static function createTables()
     {
         $seed = [
-            ['Name' => 'SuperAdmin'],
-            ['Name' => 'Admin'],
-            ['Name' => 'Moderator'],
-            ['Name' => 'Player'],
+            ['id' => 1, 'Name' => 'SuperAdmin', 'Protected' => true],
+            ['id' => 2, 'Name' => 'Admin', 'Protected' => true],
+            ['id' => 3, 'Name' => 'Moderator', 'Protected' => true],
+            ['id' => 4, 'Name' => 'Player', 'Protected' => true]
         ];
 
         Database::create('groups', function(Blueprint $table){
             $table->increments('id');
             $table->string('Name')->unique();
+            $table->boolean('Protected')->default(false);
         }, $seed);
     }
 }
