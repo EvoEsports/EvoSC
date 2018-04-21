165917d404870447a11ebdfd4be0933a31e3c7f8
Alex 'Braker' R <brakerb@vination.eu>
2018-04-21 20:28:38 +0200

    0.25.14 Fix leave message

--- core/Controllers/ChatController.php
+++ core/Controllers/ChatController.php
@@ -307,6 +307,8 @@ $$: Writes a dollarsign
             $message = '$fff' . $icon . ' ' . $message;
         }
 
+        var_dump($recipient->Login);
+
         try{
             Server::chatSendServerMessage($message, $recipient->Login);
         }catch(\Exception $e){
