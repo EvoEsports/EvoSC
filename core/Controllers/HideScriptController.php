2401c0d67c133c6cca12f8f4e31b570037c8345c
Alex 'Braker' R <brakerb@vination.eu>
2018-05-05 20:12:40 +0200

    0.32.3 Add hide script config

--- core/Controllers/HideScriptController.php
+++ core/Controllers/HideScriptController.php
@@ -4,6 +4,7 @@ namespace esc\Controllers;
 
 
 use esc\Classes\Hook;
+use esc\Classes\ManiaLinkEvent;
 use esc\Classes\Template;
 use esc\Models\Player;
 
@@ -11,22 +12,46 @@ class HideScriptController
 {
     public static function init()
     {
-        ChatController::addCommand('hidespeed', 'HideScriptController::setHideSpeed', 'Set speed at which UI hides, 0 = disable hiding');
         ChatController::addCommand('hide', 'HideScriptController::showConfig', 'Configure UI hiding. Set speed/enabled.');
 
+        ManiaLinkEvent::add('hsc.toggle', 'HideScriptController::toggle');
+        ManiaLinkEvent::add('hsc.set', 'HideScriptController::set');
+
         Hook::add('PlayerConnect', 'HideScriptController::sendHideScriptSettings');
+
+        KeyController::createBind('X', 'HideScriptController::update');
     }
 
-    public static function setHideSpeed(Player $player, $cmd = null, $hideSpeed = 0)
+    public static function update(Player $player)
     {
-        if (!$cmd || $hideSpeed === null) {
-            return;
+        TemplateController::loadTemplates();
+        self::showConfig($player);
+    }
+
+    public static function toggle(Player $player, $toggle)
+    {
+        $speed = $player->setting('ui_hide_speed') ?? 1.0;
+
+        if ($speed > 0.0) {
+            self::setHideSpeed($player, 0.0);
+        } else {
+            self::setHideSpeed($player, 500.0);
         }
 
-        $player->setSetting('ui_hide_speed', $hideSpeed * 1.0);
+        self::showConfig($player);
+    }
+
+    public static function set(Player $player, $cmd = null, $speed)
+    {
+        self::setHideSpeed($player, $speed);
+    }
+
+    public static function setHideSpeed(Player $player, $hideSpeed = 0.0)
+    {
+        $player->setSetting('ui_hide_speed', $hideSpeed / 3.8);
 
         if ($hideSpeed > 0) {
-            ChatController::message($player, '_info', 'UI hides now at ', secondary($hideSpeed));
+            ChatController::message($player, '_info', 'UI hides now at ', secondary($hideSpeed . ' km/h'));
         } else {
             ChatController::message($player, '_info', 'UI hiding disabled');
         }
@@ -43,6 +68,13 @@ class HideScriptController
     public static function showConfig(Player $player, $cmd = null)
     {
         $speed = $player->setting('ui_hide_speed') ?? 1.0;
-        Template::show($player, 'hide-config', compact('speed'));
+        Template::show($player, 'hide-config', [
+            'id' => 'ESC:ui-hide-config',
+            'title' => 'UI hiding config',
+            'width' => 60,
+            'height' => 21,
+            'showAnimation' => true,
+            'speed' => $speed
+        ]);
     }
 }
