4782b0707499ea03f811630cb981878d1af618e9
Alex 'Braker' R <brakerb@vination.eu>
2018-05-06 14:42:59 +0200

    0.32.21 Remove quick reload from donations

--- core/Modules/donations/Donations.php
+++ core/Modules/donations/Donations.php
@@ -19,14 +19,6 @@ class Donations
         Hook::add('PlayerConnect', 'Donations::show');
 
         ManiaLinkEvent::add('donate', 'Donations::donate');
-
-        KeyController::createBind('X', 'Donations::reload');
-    }
-
-    public static function reload(Player $player)
-    {
-        TemplateController::loadTemplates();
-        self::show($player);
     }
 
     public static function show(Player $player)
