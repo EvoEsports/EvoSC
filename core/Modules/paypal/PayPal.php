cc14a318d3c58f898cddca49d7ea6e0d9cf875c2
Alex 'Braker' R <brakerb@vination.eu>
2018-05-06 14:42:21 +0200

    0.32.20 Add paypal widget template

--- core/Modules/paypal/PayPal.php
+++ core/Modules/paypal/PayPal.php
@@ -3,9 +3,19 @@
 namespace esc\Modules;
 
 
+use esc\Classes\Hook;
+use esc\Classes\Template;
+use esc\Models\Player;
+
 class PayPal
 {
     public function __construct()
     {
+        Hook::add('PlayerConnect', 'PayPal::show');
+    }
+
+    public static function show(Player $player)
+    {
+        Template::show($player, 'paypal.widget');
     }
 }
