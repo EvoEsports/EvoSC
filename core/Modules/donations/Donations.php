31eb380eeedb5dd8fd7aa3f62e2b6ea4084ef77b
Alex 'Braker' R <brakerb@vination.eu>
2018-05-13 14:04:12 +0200

    0.33.23 Add donation command

--- core/Modules/donations/Donations.php
+++ core/Modules/donations/Donations.php
@@ -3,13 +3,12 @@
 namespace esc\Modules;
 
 
+use esc\Classes\ChatCommand;
 use esc\Classes\Hook;
 use esc\Classes\ManiaLinkEvent;
 use esc\Classes\Template;
 use esc\Controllers\ChatController;
-use esc\Controllers\KeyController;
 use esc\Controllers\PlanetsController;
-use esc\Controllers\TemplateController;
 use esc\Models\Player;
 
 class Donations
@@ -19,6 +18,8 @@ class Donations
         Hook::add('PlayerConnect', 'Donations::show');
 
         ManiaLinkEvent::add('donate', 'Donations::donate');
+
+        ChatCommand::add('donate', 'Donations::donateCmd', 'Donate planets to the server "/donate <amount>"');
     }
 
     public static function show(Player $player)
@@ -26,8 +27,19 @@ class Donations
         Template::show($player, 'donations.widget');
     }
 
+    public static function donateCmd(Player $player, $cmd, $amount)
+    {
+        self::donate($player, intval($amount));
+    }
+
     public static function donate(Player $player, $amount)
     {
+        if ($amount < 1) {
+            //Block donations with less then one planet
+            ChatController::message($player, '_warning', 'You can not donate less than 1 planet');
+            return;
+        }
+
         PlanetsController::createBill($player, $amount, "Donate $amount Planets?", 'Donations::paySuccess');
     }
 
