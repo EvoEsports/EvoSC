0cdfcd6eeb545f92022ea753e05c1c4820a05bf6
Alex 'Braker' R <brakerb@vination.eu>
2018-06-01 15:08:05 +0200

    0.37.2 Add better PlayerConnect

--- core/Controllers/EventController.php
+++ core/Controllers/EventController.php
@@ -3,6 +3,8 @@
 namespace esc\Controllers;
 
 
+use esc\Classes\Hook;
+use esc\Classes\Log;
 use esc\Models\Player;
 use esc\Models\Stats;
 use Illuminate\Support\Facades\DB;
@@ -20,6 +22,10 @@ class EventController
                     self::mpPlayerInfoChanged($arguments);
                     break;
 
+                case 'ManiaPlanet.PlayerConnect':
+                    self::mpPlayerConnect($arguments);
+                    break;
+
                 case 'ManiaPlanet.ModeScriptCallbackArray':
                     ModeScriptEventController::handleModeScriptCallbacks($callback);
                     break;
@@ -35,15 +41,22 @@ class EventController
     private static function mpPlayerInfoChanged($playerInfos)
     {
         foreach ($playerInfos as $playerInfo) {
-            $login = $playerInfo['Login'];
+            $login    = $playerInfo['Login'];
+            $nickname = $playerInfo['NickName'];
+            $playerId = $playerInfo['PlayerId'];
 
             $player = Player::find($login);
 
-            if (!$player) {
+            if ($player) {
+                $player->update([
+                    'NickName'  => $nickname,
+                    'player_id' => $playerId
+                ]);
+            } else {
                 $playerId = Player::insertGetId([
                     'Login'     => $login,
-                    'NickName'  => $playerInfo['NickName'],
-                    'player_id' => $playerInfo['PlayerId']
+                    'NickName'  => $nickname,
+                    'player_id' => $playerId
                 ]);
 
                 Stats::create([
@@ -51,6 +64,26 @@ class EventController
                     'Visits' => 1
                 ]);
             }
+        }
+    }
+
+    /**
+     * @param $playerInfo
+     * @throws \Exception
+     */
+    private static function mpPlayerConnect($playerInfo)
+    {
+        if (count($playerInfo) == 2 && is_string($playerInfo[0])) {
+            $login = $playerInfo[0];
+
+            try {
+                $player = Player::firstOrFail($login);
+                Hook::fire('PlayerConnect', $player);
+            } catch (\Exception $e) {
+                Log::logAddLine('EventController', "Error: Player ($login) not found!");
+            }
+        } else {
+            throw new \Exception('Malformed callback');
         }
     }
 }
