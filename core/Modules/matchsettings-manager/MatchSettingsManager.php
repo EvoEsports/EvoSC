ab38973c4a5134c61f01e59ec78c793982e902ec
Alex 'Braker' R <brakerb@vination.eu>
2018-08-24 14:48:00 +0200

    0.40.12 Add duplicate, delete, load match-setting

--- core/Modules/matchsettings-manager/MatchSettingsManager.php
+++ core/Modules/matchsettings-manager/MatchSettingsManager.php
@@ -4,19 +4,30 @@ namespace esc\Modules;
 
 
 use esc\Classes\File;
-use esc\Classes\MatchSettings;
+use esc\Classes\Log;
+use esc\Classes\ManiaLinkEvent;
+use esc\Classes\Server;
 use esc\Classes\Template;
 use esc\Controllers\ChatController;
 use esc\Controllers\KeyController;
 use esc\Controllers\TemplateController;
 use esc\Models\Player;
+use esc\Modules\MapList\MapList;
 use Illuminate\Support\Collection;
 
 class MatchSettingsManager
 {
+    private static $path;
+
     public static function init()
     {
+        self::$path = config('server.base') . '/UserData/Maps/MatchSettings/';
+
         ChatController::addCommand('ms', [self::class, 'showMatchSettingsOverview'], 'Show MatchSettingsManager', '//', 'ms.edit');
+
+        ManiaLinkEvent::add('msm.delete', [self::class, 'deleteMatchSetting']);
+        ManiaLinkEvent::add('msm.duplicate', [self::class, 'duplicateMatchSettings']);
+        ManiaLinkEvent::add('msm.load', [self::class, 'loadMatchSettings']);
 
         KeyController::createBind('Y', [self::class, 'showMatchSettingsOverview']);
     }
@@ -24,7 +35,10 @@ class MatchSettingsManager
     public static function showMatchSettingsOverview(Player $player)
     {
         TemplateController::loadTemplates();
-        $settings = self::getMatchSettings();
+
+        $settings = self::getMatchSettings()->map(function (String $file) {
+            return preg_replace('/\.txt$/', '', $file);
+        });
 
         Template::show($player, 'matchsettings-manager.overview', compact('settings'));
     }
@@ -37,11 +51,46 @@ class MatchSettingsManager
         return $files;
     }
 
-    public static function loadMatchSettings(string $file): ?MatchSettings
+    public static function deleteMatchSetting(Player $player, string $matchSettingsFile)
     {
-        $path = config('server.base') . '/UserData/Maps/MatchSettings/';
-        $contents = File::get($path . $file);
+        $file = self::$path . $matchSettingsFile . '.txt';
+        File::delete($file);
+        self::showMatchSettingsOverview($player);
 
-        return new MatchSettings($contents);
+        Log::logAddLine('MatchSettingsManager', "$player deleted MatchSettingsFile: $matchSettingsFile");
+    }
+
+    public static function loadMatchSettings(Player $player, string $matchSettingsFile)
+    {
+        $file = 'MatchSettings/' . $matchSettingsFile . '.txt';
+        Server::loadMatchSettings($file);
+
+        //Update maps
+        onlinePlayers()->each([MapList::class, 'sendManialink']);
+
+        ChatController::messageAll($player->group, ' ', $player->NickName, ' loads new settings ', secondary($matchSettingsFile));
+
+        Log::logAddLine('MatchSettingsManager', "$player loads MatchSettings: $matchSettingsFile");
+    }
+
+    public static function duplicateMatchSettings(Player $player, string $name)
+    {
+        $files = self::getMatchSettings();
+
+        $file = $files->map(function (string $file) use ($name) {
+            if (preg_match("/$name - Copy \((\d+)\)/", $file, $matches)) {
+                return $name . ' - Copy (' . (intval($matches[1]) + 1) . ')';
+            }
+        })->filter()->last();
+
+        if (!$file) {
+            $file = $name . ' - Copy (1)';
+        }
+
+        $originalFile = self::$path . $name . '.txt';
+        File::put(self::$path . $file . '.txt', File::get($originalFile));
+        self::showMatchSettingsOverview($player);
+
+        Log::logAddLine('MatchSettingsManager', "$player duplicated MatchSettingsFile: $matchSettingsFile");
     }
 }
