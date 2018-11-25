d5243ffc811aaee2fa4ffb268aae71bfe8412592
Alex 'Braker' R <brakerb@vination.eu>
2018-11-25 18:01:57 +0100

    0.45.5 Add draggable frames

---
+++ core/Classes/ManiaLinkDrag.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace esc\Classes;
+
+
+class ManiaLinkDrag
+{
+    public $targetId;
+
+    public function __construct($targetId = "handle")
+    {
+        $this->targetId = $targetId;
+    }
+
+    public function __toString()
+    {
+        return '
+Void maniaLinkDrag(){
+    declare frame <=> (Page.MainFrame.GetFirstChild("' . $this->targetId . '") as CMlFrame);
+    
+    declare framePos = frame.AbsolutePosition_V3;
+    declare frameSize = frame.Size;
+    
+    if(MouseX >= framePos[0] && MouseX <= (framePos[0] + frameSize[0]) && MouseY >= (framePos[1] - frameSize[1]) && MouseY <= framePos[1] && MouseLeftButton){
+        declare Real ZIndex for LocalUser = 305.0;
+        declare startPos = frame.Parent.RelativePosition_V3;
+        declare startX = MouseX;
+        declare startY = MouseY;
+        
+        if(frame.Parent.ZIndex > ZIndex){
+            ZIndex = frame.Parent.ZIndex;
+        }
+        
+        ZIndex = ZIndex + 1.0;
+        frame.Parent.ZIndex = ZIndex;
+        
+        while(MouseLeftButton){
+            yield;
+            
+            declare newPosX = startPos[0] + (MouseX - startX);
+            declare newPosY = startPos[1] + (MouseY - startY);
+            
+            frame.Parent.RelativePosition_V3 = <newPosX, newPosY>;
+        }
+    }
+    
+}';
+    }
+}
