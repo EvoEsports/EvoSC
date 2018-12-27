79f4cb9dac20eb34acafdd05c9c1433aa4bd4bd6
Alex 'Braker' R <brakerb@vination.eu>
2018-12-27 12:06:44 +0100

    0.47.11 Sort cp tracker descending and do not clear on cp reset

--- core/Modules/cp-position-tracker/CpPositionTracker.php
+++ core/Modules/cp-position-tracker/CpPositionTracker.php
@@ -5,6 +5,8 @@ namespace esc\Modules;
 
 use esc\Classes\Hook;
 use esc\Classes\Template;
+use esc\Controllers\KeyController;
+use esc\Controllers\TemplateController;
 use esc\Models\Player;
 
 class CpPositionTracker
@@ -14,10 +16,13 @@ class CpPositionTracker
         if (config('cp-pos-tracker.enabled')) {
             Hook::add('PlayerConnect', [self::class, 'showManialink']);
         }
+
+        KeyController::createBind('X', [self::class, 'showManialink']);
     }
 
     public static function showManialink(Player $player)
     {
+        TemplateController::loadTemplates();
         Template::show($player, 'cp-position-tracker.manialink');
     }
 }
