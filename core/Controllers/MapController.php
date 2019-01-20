18c1f15024190ca860f5cfbe51a788bb9b59c575
Alex 'Braker' R <brakerb@vination.eu>
2019-01-20 17:43:09 +0100

    0.50.21 Fix addtime

--- core/Controllers/MapController.php
+++ core/Controllers/MapController.php
@@ -94,7 +94,7 @@ class MapController implements ControllerInterface
     public static function addTime(int $minutes = 10)
     {
         self::$addedTime += $minutes;
-        $totalNewTime    = (self::$timeLimit + self::$addedTime);
+        $totalNewTime    = (self::$timeLimit * 10 + self::$addedTime * 60);
         self::updateRoundtime($totalNewTime);
     }
 
