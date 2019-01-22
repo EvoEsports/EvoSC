20ba5a5b75fdfb6b8d4b932310af20b07dc2ea1b
Alex 'Braker' R <brakerb@vination.eu>
2019-01-22 16:07:44 +0100

    0.50.29 Remove auto afk manialink

--- core/Modules/auto-afk/AutoAfk.php
+++ core/Modules/auto-afk/AutoAfk.php
@@ -18,22 +18,6 @@ class AutoAfk
 
     public function __construct()
     {
-        // KeyController::createBind('X', [self::class, 'reload']);
-
-        Hook::add('PlayerConnect', [self::class, 'showManialink']);
-
-        ManiaLinkEvent::add('afk', [self::class, 'setAfk']);
-    }
-
-    public static function reload(Player $player)
-    {
-        TemplateController::loadTemplates();
-        Template::show($player, 'auto-afk.manialink');
-    }
-
-    public static function showManialink(Player $player)
-    {
-        Template::show($player, 'auto-afk.manialink');
     }
 
     public static function setAfk(Player $player)
@@ -51,5 +35,22 @@ class AutoAfk
         }
 
         ChatController::message(onlinePlayers(), $player, ' was moved to spectators after ', secondary(config('auto-afk.minutes') . ' minutes'), ' of racing inactivity.');
+    }
+
+    public static function forceAfk(Player $player, Player $admin)
+    {
+        try {
+            Server::forceSpectator($player->Login, 3);
+        } catch (Exception $e) {
+            Log::logAddLine('AutoAfk', $e->getMessage());
+        }
+
+        try {
+            Server::forceSpectatorTarget($player->Login, "", 2);
+        } catch (Exception $e) {
+            Log::logAddLine('AutoAfk', $e->getMessage());
+        }
+
+        ChatController::message(onlinePlayers(), $player, ' was forced to spectators by ', $admin);
     }
 }
