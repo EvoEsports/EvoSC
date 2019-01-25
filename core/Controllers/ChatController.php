b8ba5cc6f54d60addf1a1124b880d085817b1ffc
Alex 'Braker' R <brakerb@vination.eu>
2019-01-25 09:50:12 +0100

    0.51.4 Fix color of starting bracket

--- core/Controllers/ChatController.php
+++ core/Controllers/ChatController.php
@@ -128,7 +128,7 @@ $$: Writes a dollarsign
 
         $prefix   = $player->group->chat_prefix;
         $color    = $player->group->color ?? config('colors.chat');
-        $chatText = sprintf('[$z$s%s$z$s$%s] $%s$z$s%s', $nick, $color, config('colors.chat'), $text);
+        $chatText = sprintf('$%s[$z$s%s$z$s$%s] $%s$z$s%s', $color, $nick, $color, config('colors.chat'), $text);
 
         if ($prefix) {
             $chatText = '$' . $color . $prefix . ' ' . $chatText;
