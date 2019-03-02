528ae48bd30608209471973a768fa6c5c44167fd
Alex 'Braker' R <brakerb@vination.eu>
2019-03-02 16:59:01 +0100

    0.53.2 Show added time to newly connected players

--- core/Modules/added-time-info/AddedTimeInfo.php
+++ core/Modules/added-time-info/AddedTimeInfo.php
@@ -4,18 +4,15 @@ namespace esc\Modules;
 
 
 use esc\Classes\Hook;
-use esc\Classes\Server;
 use esc\Classes\Template;
-use esc\Controllers\KeyController;
 use esc\Controllers\MapController;
-use esc\Controllers\TemplateController;
-use esc\Models\Map;
 use esc\Models\Player;
 
 class AddedTimeInfo
 {
     public function __construct()
     {
+        Hook::add('PlayerConnect', [self::class, 'playerConnect']);
         Hook::add('EndMatch', [self::class, 'endMatch']);
         Hook::add('TimeLimitUpdated', [self::class, 'timeLimitUpdated']);
     }
@@ -24,5 +21,11 @@ class AddedTimeInfo
     {
         $addedMinutes = floor(MapController::getAddedTime() / 60);
         Template::showAll('added-time-info.meter', compact('addedMinutes'));
+    }
+
+    public static function playerConnect(Player $player, $timeLimitInSeconds)
+    {
+        $addedMinutes = floor(MapController::getAddedTime() / 60);
+        Template::show($player, 'added-time-info.meter', compact('addedMinutes'));
     }
 }
