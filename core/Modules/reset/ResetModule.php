f67943a6d96b593ec317235c96a2da0140463d1f
Alex 'Braker' R <brakerb@vination.eu>
2019-03-20 15:52:53 +0100

    0.60.11 Add reset all command

--- core/Modules/reset/ResetModule.php
+++ core/Modules/reset/ResetModule.php
@@ -12,10 +12,18 @@ class ResetModule
     public function __construct()
     {
         ChatCommand::add('/reset', [self::class, 'reset'], 'Reset the UI in case it broke.');
+        ChatCommand::add('//resetall', [self::class, 'resetAll'], 'Reset the UI in case it broke.', 'config');
     }
 
     public static function reset(Player $player)
     {
         Hook::fire('PlayerConnect', $player);
+    }
+
+    public static function resetAll(Player $player)
+    {
+        onlinePlayers()->each(function (Player $player) {
+            Hook::fire('PlayerConnect', $player);
+        });
     }
 }
