6a6c03f2749649fac4581565f4e280482c26dd4e
Alex 'Braker' R <brakerb@vination.eu>
2019-04-22 17:15:46 +0200

    0.63.28 Test

--- core/Modules/_tester/TestModule.php
+++ core/Modules/_tester/TestModule.php
@@ -4,6 +4,8 @@
 namespace esc\Modules;
 
 
+use esc\Classes\Log;
+use esc\Classes\Server;
 use esc\Classes\Template;
 use esc\Controllers\TemplateController;
 use esc\Models\Player;
@@ -12,12 +14,11 @@ class TestModule
 {
     public function __construct()
     {
-        // KeyBinds::add('test_stuff', '', [self::class, 'testStuff'], 'X', 'ma');
+        KeyBinds::add('test_stuff', '', [self::class, 'testStuff'], 'X', 'ma');
     }
 
     public static function testStuff(Player $player)
     {
-        TemplateController::loadTemplates();
-        Template::show($player, '_tester.test');
+        Log::logAddLine('test', json_encode(Server::getDetailedPlayerInfo($player->Login)));
     }
 }
