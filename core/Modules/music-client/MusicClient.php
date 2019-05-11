be458de92162168aed2c073aae1ff8b2cb1f7323
Alex 'Braker' R <brakerb@vination.eu>
2019-05-11 13:27:49 +0200

    0.65.3 Add music command

--- core/Modules/music-client/MusicClient.php
+++ core/Modules/music-client/MusicClient.php
@@ -2,6 +2,7 @@
 
 namespace esc\Modules\MusicClient;
 
+use esc\Classes\ChatCommand;
 use esc\Classes\Hook;
 use esc\Classes\Template;
 use esc\Controllers\TemplateController;
@@ -14,6 +15,8 @@ class MusicClient
     {
         Hook::add('PlayerConnect', [self::class, 'playerConnect']);
 
+        ChatCommand::add('/music', [self::class, 'searchMusic'], 'Open and search the music list.');
+
         KeyBinds::add('reload_music_client', 'Reload music client.', [self::class, 'reload'], 'F2', 'ms');
     }
 
@@ -24,12 +27,18 @@ class MusicClient
      */
     public static function playerConnect(Player $player)
     {
-        Template::show($player, 'music-client.widget');
+        Template::show($player, 'music-client.music-client');
     }
 
     public static function reload(Player $player)
     {
         TemplateController::loadTemplates();
         self::playerConnect($player);
+    }
+
+    public static function searchMusic(Player $player, $cmd, ...$arguments)
+    {
+        $query = implode(' ', $arguments);
+        Template::show($player, 'music-client.search-command', compact('query'));
     }
 }
