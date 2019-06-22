2e5ad49a564e9799019ae7ad8d7e1e6318acaf57
Alex 'Braker' R <brakerb@vination.eu>
2019-06-22 14:46:49 +0200

    0.70.0 Add hide script template

--- core/Modules/_tester/TestModule.php
+++ core/Modules/_tester/TestModule.php
@@ -21,6 +21,6 @@ class TestModule
     public static function testStuff(Player $player = null)
     {
         TemplateController::loadTemplates();
-        MatchSettingsManager::showEditMatchsettings($player, 'drtk01.ms');
+        LiveRankings::playerConnect($player);
     }
 }
