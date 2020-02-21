4cc3749c140cf87fd593bde9a6015940172fbc38
Alex 'Braker' R <brakerb@vination.eu>
2020-02-21 18:07:06 +0100

    0.80.16 Add fix locals ranks command

---
+++ core/Commands/FixRanks.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace esc\Commands;
+
+use esc\Classes\Database;
+use esc\Classes\DB;
+use esc\Classes\Log;
+use esc\Controllers\ConfigController;
+use esc\Models\Map;
+use esc\Models\Player;
+use esc\Modules\LocalRecords\LocalRecords;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FixRanks extends Command
+{
+    protected function configure()
+    {
+        $this->setName('fix:local-ranks')
+            ->setDescription('Recalculate all ranks for the local records.');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        ConfigController::init();
+        Log::setOutput($output);
+        Database::init();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $maps = Map::all();
+        $bar = new ProgressBar($output, $maps->count());
+        $bar->start();
+
+        foreach ($maps as $map) {
+            LocalRecords::fixRanks($map);
+            $bar->advance();
+        }
+
+        $bar->finish();
+    }
+
+    private static function fixRanks(Map $map)
+    {
+        DB::raw('SET @rank=0');
+        DB::raw('UPDATE `local-records` SET `Rank`= @rank:=(@rank+1) WHERE `Map` = ' . $map->id . ' ORDER BY `Score`');
+    }
+}
