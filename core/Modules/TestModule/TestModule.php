597f31bfea963eb0f83a7e2f45c4363c864269a4
Alex 'Braker' R <brakerb@vination.eu>
2020-05-19 18:28:33 +0200

    0.85.15 Emit TestModule class to prevent warnings

--- core/Modules/TestModule/TestModule.php
+++ core/Modules/TestModule/TestModule.php
@@ -1,19 +1,16 @@
 <?php
 
 
-namespace esc\Modules;
+namespace EvoSC\Modules\TestModule;
 
 
-use EvoSC\Classes\Log;
 use EvoSC\Classes\ManiaLinkEvent;
 use EvoSC\Classes\Module;
-use EvoSC\Classes\Server;
 use EvoSC\Classes\Template;
 use EvoSC\Controllers\TemplateController;
 use EvoSC\Interfaces\ModuleInterface;
 use EvoSC\Models\Player;
 use EvoSC\Modules\InputSetup\InputSetup;
-use EvoSC\Modules\Reset\Reset;
 use Illuminate\Support\Collection;
 
 class TestModule extends Module implements ModuleInterface
@@ -31,7 +28,7 @@ class TestModule extends Module implements ModuleInterface
     public static function testStuff(Player $player = null)
     {
         TemplateController::loadTemplates();
-        AprilFools::show($player);
+        restart_evosc();
     }
 
     public static function sendTestManialink(Player $player)
