a034548f2a12703d5ffd345e3b6325a71b8c91b7
Alex 'Braker' R <brakerb@vination.eu>
2020-12-21 18:43:23 +0100

    0.97.15 Add default value for team column

---
+++ Migrations/1608552521_add-teams-column-to-players-table.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace EvoSC\Migrations;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+class AddTeamsColumnToPlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(Builder $schemaBuilder)
+    {
+        $schemaBuilder->table('players', function (Blueprint $table) {
+            $table->tinyInteger('team')->default(-1)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(Builder $schemaBuilder)
+    {
+        $schemaBuilder->table('players', function (Blueprint $table) {
+        });
+    }
+}
