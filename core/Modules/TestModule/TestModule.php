5920ab0ab0f8d61e367ff181a7065a235c8eb9d1
Alex 'Braker' R <brakerb@vination.eu>
2020-12-29 17:09:21 +0100

    0.97.30 Enable FloatingNicknames

--- core/Modules/TestModule/TestModule.php
+++ core/Modules/TestModule/TestModule.php
@@ -10,6 +10,7 @@ use EvoSC\Classes\Template;
 use EvoSC\Controllers\TemplateController;
 use EvoSC\Interfaces\ModuleInterface;
 use EvoSC\Models\Player;
+use EvoSC\Modules\FloatingNickNames\FloatingNickNames;
 use EvoSC\Modules\HackMe\HackMe;
 use EvoSC\Modules\InputSetup\InputSetup;
 use EvoSC\Modules\LiveRankings\LiveRankings;
@@ -34,7 +35,7 @@ class TestModule extends Module implements ModuleInterface
     public static function testStuff(Player $player = null)
     {
         TemplateController::loadTemplates();
-        TeamInfo::showWidget($player);
+        FloatingNickNames::sendScript($player);
     }
 
     public static function sendTestManialink(Player $player)
