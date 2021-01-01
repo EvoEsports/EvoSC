28f6a5374d01d1d0b3f84bacec3a0a8bf2eb6dfe
Alex 'Braker' R <brakerb@vination.eu>
2021-01-01 13:49:39 +0100

    0.98.13 Fix round counter frozen in rounds mode

--- core/Modules/TestModule/TestModule.php
+++ core/Modules/TestModule/TestModule.php
@@ -38,7 +38,7 @@ class TestModule extends Module implements ModuleInterface
     public static function testStuff(Player $player = null)
     {
         TemplateController::loadTemplates();
-        FloatingNickNames::sendScript($player);
+        MatchRounds::showWidget($player);
     }
 
     public static function sendTestManialink(Player $player)
