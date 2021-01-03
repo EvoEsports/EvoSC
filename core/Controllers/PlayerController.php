e45069edcb137bf24bdfedc645787a94c449b531
Alex 'Braker' R <brakerb@vination.eu>
2021-01-03 12:50:39 +0100

    0.98.26 Send custom names on player connect

--- core/Controllers/PlayerController.php
+++ core/Controllers/PlayerController.php
@@ -223,6 +223,8 @@ class PlayerController implements ControllerInterface
             }
             self::announceConnect($player, $name);
         });
+
+        self::sendUpdatedCustomNames();
     }
 
     /**
