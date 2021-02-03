d3e2f557c6e0ee803266ec26a068c6fbf7d7b2c0
Alex 'Braker' R <brakerb@vination.eu>
2021-02-03 20:22:26 +0100

    0.98.49 Send hidescript on player connect

--- core/Modules/HideScript/HideScript.php
+++ core/Modules/HideScript/HideScript.php
@@ -4,6 +4,7 @@
 namespace EvoSC\Modules\HideScript;
 
 
+use EvoSC\Classes\Hook;
 use EvoSC\Classes\Module;
 use EvoSC\Classes\Template;
 use EvoSC\Interfaces\ModuleInterface;
@@ -11,11 +12,20 @@ use EvoSC\Models\Player;
 
 class HideScript extends Module implements ModuleInterface
 {
+    /**
+     * @param string $mode
+     * @param bool $isBoot
+     * @return mixed|void
+     */
     public static function start(string $mode, bool $isBoot = false)
     {
-        // TODO: Implement start() method.
+        Hook::add('PlayerConnect', [self::class, 'sendScript']);
     }
 
+    /**
+     * @param Player $player
+     * @throws \EvoSC\Exceptions\InvalidArgumentException
+     */
     public static function sendScript(Player $player)
     {
         Template::show($player, 'HideScript.script');
